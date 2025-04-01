0c3c3b4d6619ee69535e43caf45de99a75571d8d
ranjithtechie <ranjithwebwiz@gmail.com>
2025-04-01 13:35:18 +0530

    fix:manual bypass ssll

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,4 +5,5 @@ export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API  || "https://localhost:5001/api",
 
     withCredentials: true,
+    rejectUnauthorized: false,
 });
