e41c48be03be003af13ce1d96e398cad082d7b90
ranjithtechie <ranjithwebwiz@gmail.com>
2025-04-01 11:09:20 +0530

    chore:created env vars to host

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.VITE_API  || "http://localhost:5001/api",
 
     withCredentials: true,
 });
